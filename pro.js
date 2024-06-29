0143b58acd75c507fd3039cabcda56ecc0495468
likhithalavanya <119840461+likhithalavanya@users.noreply.github.com>
2024-06-29 22:39:35 +0530

    Add files via upload

---
+++ pro.js
@@ -0,0 +1,12 @@
+function validateForm() {
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirmPassword').value;
+
+    if (password !== confirmPassword) {
+        alert('Passwords do not match!');
+        return false;
+    }
+
+    alert('Registration completed successfully!');
+    return true;
+}
